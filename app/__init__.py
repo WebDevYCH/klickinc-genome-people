9521d5ffc4988655f3e165a6c97938c2fca93421
webdevych <lee.smith870327@gmail.com>
2022-11-04 21:43:40 -0400

    connects to db now! has crud interfaces!

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import config
-
-# Create application and db references
-app = config.configapp(Flask(__name__))
-db = SQLAlchemy(app)
-
