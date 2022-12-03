35a94ef4174a2dde2ceff2d013e28ba62e2518df
meticulousfan <elithofan@gmail.com>
2022-12-03 03:38:36 +0000

    added: profil, skill edit page

--- app/profile.py
+++ app/profile.py
@@ -1,75 +1,73 @@
-import datetime
-import requests
-
-from flask import Flask, render_template, flash, redirect, jsonify, json, url_for, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_bootstrap import Bootstrap
-from flask_login import LoginManager, login_required, current_user
-from flask_admin import expose
-from flask_admin.menu import MenuCategory, MenuView, MenuLink, SubMenuCategory
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_required, current_user
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, IntegerField, RadioField, SelectMultipleField, TextAreaField, widgets
-from wtforms.validators import InputRequired, Length
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy import MetaData, delete, insert, update, or_, and_
-from sqlalchemy.exc import IntegrityError
+from wtforms import SelectField, TextAreaField, SelectMultipleField
+from wtforms.validators import InputRequired
 
 from core import *
 from model import *
+from helpers import *
 
 @app.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
     """Route to the job."""
     form = ProfileForm()
-    profile = db.session.query(UserProfile).filter(UserProfile.user_id==current_user.userid).one()
+    profile = db.session.query(UserProfile).filter(UserProfile.user_id==current_user.userid).first()
+    my_skills = db.session.query(Skill).join(UserSkill).where(UserSkill.user_id == current_user.userid).all()
     skillform = SkillForm()
-    lightcast_response = get_allskills_from_lightcast()
-    print(lightcast_response)
+    # fill_skills_by_lightcast_api()
     if form.validate_on_submit():
         if profile:
+            relevant_skills = extract_skills_from_text(form.resume.data)
+            auto_fill_user_skill_from_resume(relevant_skills)
             profile.resume = form.resume.data
             db.session.commit()
-            flash("Successfully updated your resume!")
+            flash("Successfully updated!")
         else:
             user_profile = UserProfile(resume=form.resume.data, user_id=int(current_user.userid))
             db.session.add(user_profile)
             db.session.commit()
             flash("Successfully added your resume")
+    return render_template('profile/index.html', form=form, skillform=skillform, profile=profile, user=current_user, skills=my_skills)
+
+@app.route('/skill-edit', methods=['GET', 'POST'])
+@login_required
+def skill_edit():
+    skillform = SkillForm()
+    my_skills = db.session.query(Skill).join(UserSkill).where(UserSkill.user_id == current_user.userid).all()
+    return render_template('profile/skill_edit.html', skillform=skillform, skills=my_skills)
+
+@app.route('/skill-add', methods=['POST'])
+@login_required
+def skill_add():
+    skillform = SkillForm()
     if skillform.validate_on_submit():
-        skill = db.session.query(UserSkill).query(UserSkill.user)
-    return render_template('profile/index.html', form=form, skillform=skillform, profile=profile, user=current_user)
+        for id in skillform.skillid.data:
+            if not db.session.query(UserSkill).filter(UserSkill.user_id==current_user.userid, UserSkill.skill_id==id).first():
+                new_skill = UserSkill(skill_id=id, user_id=int(current_user.userid), user_skill_source_id=1)
+                db.session.add(new_skill)
+        db.session.commit()
+        flash("Successfully added your skills!") 
+    return redirect(url_for('skill_edit'))
 
-def get_lightcast_auth_token():
-    url = "https://auth.emsicloud.com/connect/token"
-    payload = "client_id=" + app.config['LIGHTCAST_API_CLIENTID'] + "&client_secret=" + app.config['LIGHTCAST_API_SECRET'] + "&grant_type=client_credentials&scope=emsi_open"
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-
-    response = requests.request("POST", url, data=payload, headers=headers)
-
-    return response.json()['access_token']
-
-def get_allskills_from_lightcast():
-    url = "https://emsiservices.com/skills/versions/latest/skills"
-
-    querystring = {"q":".NET","typeIds":"ST1,ST2","fields":"id,name,type,infoUrl","limit":"5"}
-
-    bearer_token = 'Bearer' + get_lightcast_auth_token()
-    headers = {'Authorization': bearer_token}
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-
-    return response.json()
+@app.route('/profile/skill-edit/delete/<id>')
+@login_required
+def skill_remove(id):
+    db.session.query(UserSkill).filter(UserSkill.user_id==current_user.userid, UserSkill.skill_id==id).delete(synchronize_session="fetch")
+    db.session.commit()
+    flash("Successfully removed")
+    return redirect(url_for('skill_edit'))
 
 class ProfileForm(FlaskForm):
     resume = TextAreaField('description', validators=[InputRequired()])
 
 class SkillForm(FlaskForm):
-    skillid = SelectField('skillid', choices=[])
+    skillid = SelectMultipleField('skillid', choices=[], coerce=int)
 
-    def __init__(self, *args, **kwargs):
-        super(SkillForm, self).__init__(*args, **kwargs)
+    def __init__(self, validate_choice=True, *args, **kwargs):
+        super(SkillForm, self).__init__(validate_choice=validate_choice, *args, **kwargs)
         SKILL_CHOICES = []
         skills = db.session.query(Skill).all()
         for x in skills:
