425197aa5265b47c6220c5d34385ada6a46074f2
meticulousfan <elithofan@gmail.com>
2022-12-08 09:06:47 +0000

    Added: autofill skill db replication

--- app/profile.py
+++ app/profile.py
@@ -14,12 +14,10 @@ from helpers import *
 def profile():
     """Route to the job."""
     profile = db.session.query(UserProfile).filter(UserProfile.user_id==current_user.userid).one()
-    my_skills = db.session.query(Skill).join(UserSkill).where(UserSkill.user_id == current_user.userid).all()
+    my_skills = db.session.query(Skill).join(UserSkill).where(UserSkill.user_id == current_user.userid).order_by(Skill.name).all()
 
     if request.method == "POST":
         if profile:
-            relevant_skills = extract_skills_from_text(request.form["resume"])
-            auto_fill_user_skill_from_resume(relevant_skills)
             profile.resume = request.form["resume"]
             db.session.commit()
             flash("Successfully updated your resume")
@@ -28,18 +26,23 @@ def profile():
             db.session.add(user_profile)
             db.session.commit()
             flash("Successfully added your resume")
+        relevant_skills = extract_skills_from_text(request.form["resume"])
+        if "message" in relevant_skills:
+            flash("Something error happened! Please wait.")
+        else:
+            auto_fill_user_skill_from_resume(relevant_skills['data'])
     return render_template('profile/index.html', profile=profile, user=current_user, skills=my_skills)
 
 @app.route('/profile/edit-skills')
 @login_required
 def resume_skills():
-    skills = db.session.query(Skill).join(UserSkill).filter(UserSkill.user_id==current_user.userid).all()
+    skills = db.session.query(Skill).join(UserSkill).filter(UserSkill.user_id==current_user.userid).order_by(Skill.name).all()
     return render_template('profile/skill_edit.html', title="Personal Skills", skills=skills)
 
 @app.route('/profile/total-skills-data')
 @login_required
 def total_skills_data():
-    skills = db.session.query(Skill).all()
+    skills = db.session.query(Skill).order_by(Skill.name).all()
     return [{"id": skill.id, "value": skill.name} for skill in skills]
 
 @app.route('/profile/skill-add', methods=['POST'])
@@ -47,7 +50,6 @@ def total_skills_data():
 def skill_add():
     if request.method == "POST":
         newSkills = request.form["newSkills"].split(",")
-        print(newSkills)
         for id in newSkills:
             if not db.session.query(UserSkill).filter(UserSkill.user_id==current_user.userid, UserSkill.skill_id==int(id)).first():
                 new_skill = UserSkill(skill_id=int(id), user_id=int(current_user.userid), user_skill_source_id=1)
