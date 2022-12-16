067589c5d2b41dd407bc478a2acc59f5153da57b
WebDevYCH <lee.smith870327@gmail.com>
2022-12-17 03:36:20 +0800

    Fixing skills set fetched using lightcast API

--- app/jobads.py
+++ app/jobads.py
@@ -2,11 +2,18 @@ from sqlite3 import Row
 from flask_login import login_required
 from datetime import date
 import json
+import requests
 from sqlalchemy.orm import joinedload
 from sqlalchemy.ext.declarative import DeclarativeMeta
 from core import *
 from model import *
+from core import app
+def row2dict(row):
+    d = {}
+    for column in row.__table__.columns:
+        d[column.name] = str(getattr(row, column.name))
 
+    return d
 admin.add_view(AdminModelView(JobPosting, db.session, category='Job Ads'))
 
 @app.route('/jobads/postjob', methods=['GET', 'POST'])
@@ -14,15 +21,35 @@ admin.add_view(AdminModelView(JobPosting, db.session, category='Job Ads'))
 def postjob():
     title = request.form['title']
     category_id = request.form['category_id']
-    description = request.form['description']
+    job_description = request.form['description']
     poster_id = request.form['poster_id']
     posted_date = date.today()
     expiry_date = request.form['expiry_date']
-    db.session.execute(
-        insert(JobPosting).
-        values(job_posting_category_id=category_id, poster_user_id=poster_id, posted_date=posted_date, expiry_date=expiry_date,title=title, description=description)
-    )
+
+    createJob = JobPosting(job_posting_category_id=category_id, poster_user_id=poster_id, posted_date=posted_date, expiry_date=expiry_date,title=title, description=job_description)
+    db.session.add(createJob)
     db.session.commit()
+    
+    datas = skillSearchAuth(job_description)
+
+    for data in datas:
+        skill_name = data['skill']['name']
+        skill_description = data['skill']['description']
+
+        db_skill = db.session.query(Skill).filter_by(name = skill_name).first()
+        if(db_skill):
+            createSkill = JobPostingSkill(job_posting_id = createJob.id, skill_id = db_skill.id)
+            db.session.add(createSkill)
+            db.session.commit()
+        else:
+            print('no db skill')
+            create_skill = Skill(name = skill_name, description = skill_description)
+            db.session.add(create_skill)
+            db.session.commit()
+            createSkill = JobPostingSkill(job_posting_id = createJob.id, skill_id = create_skill.id)
+            db.session.add(createSkill)
+            db.session.commit()
+
     return redirect(url_for('jobsearch'))
 
 @app.route('/jobads/editjob', methods=['GET', 'POST'])
@@ -42,10 +69,45 @@ def editjob():
     db.session.commit()
     return redirect(url_for('jobsearch'))
 
+@app.route('/jobads/skillsearchauth', methods=['GET', 'POST'])
+@login_required
+def skillSearchAuth(description):
+
+    url = "https://auth.emsicloud.com/connect/token"
+
+    payload = "client_id="+app.config['LIGHTCAST_API_CLIENTID']+"&client_secret="+app.config['LIGHTCAST_API_SECRET']+"&grant_type=client_credentials&scope="+app.config['LIGHTCAST_API_SCOPE']
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    response = requests.request("POST", url, data=payload, headers=headers)
+    token = json.loads(response.text)
+
+    # for test
+    return (skillSearch(token['access_token'], description))
+
+@app.route('/jobads/skillsearch', methods=['GET', 'POST'])
+@login_required
+def skillSearch(token, data):
+    url = "https://emsiservices.com/skills/versions/latest/extract"
+
+    querystring = {"language":"en"}
+
+    payload = {}
+    payload['text'] = data
+    payload['confidenceThreshold'] = 0.6
+
+    headers = {
+        'Authorization': "Bearer "+ token,
+        'Content-Type': "application/json"
+        }
+    response = requests.request("POST", url, data=json.dumps(payload), headers=headers, params=querystring)
+    result = json.loads(response.text)
+    return(result['data'])
+
 @app.route('/jobads/jobsearch', methods=['GET', 'POST'])
 @login_required
 def jobsearch():
     categories = db.session.query(JobPostingCategory).order_by(JobPostingCategory.name).all()
+
+    # skill = db.session.query(Skill).all()
     titles = db.session.query(Title).order_by(Title.name).all()
     csts = db.session.query(User.cst).filter(User.enabled == True).distinct().order_by(User.cst).all()
     csts = [row.cst for row in csts]
@@ -57,34 +119,58 @@ def jobsearch():
         category_id = int(request.form['category_id'])
         title = request.form['title']
         if(category_id == 0 and title != 'Select job title'):
-            jobs = db.session.query(JobPosting).join(JobPostingCategory).filter(today-JobPosting.posted_date<delta, JobPosting.title==title).all()
+            jobs = db.session.query(JobPosting).filter(today-JobPosting.posted_date<delta, JobPosting.title==title).all()
         elif(category_id != 0 and title == 'Select job title'):
-            jobs = db.session.query(JobPosting).join(JobPostingCategory).filter(today-JobPosting.posted_date<delta, JobPosting.job_posting_category_id==category_id).all() 
+            jobs = db.session.query(JobPosting).filter(today-JobPosting.posted_date<delta, JobPosting.job_posting_category_id==category_id).all() 
         elif(category_id == 0 and title == 'Select job title'):
-            jobs = db.session.query(JobPosting).join(JobPostingCategory).filter(today-JobPosting.posted_date<delta).all()
+            jobs = db.session.query(JobPosting).filter(today-JobPosting.posted_date<delta).all()
         else:
-            jobs = db.session.query(JobPosting).join(JobPostingCategory).filter(today-JobPosting.posted_date<delta, JobPosting.job_posting_category_id==category_id, JobPosting.title==title).all()
+            jobs = db.session.query(JobPosting).filter(today-JobPosting.posted_date<delta, JobPosting.job_posting_category_id==category_id, JobPosting.title==title).all()
        
-        data = []
+        result = []   
         for job in jobs:
-            # get json file from database
-            data_job = {i:v for i, v in job.__dict__.items() if i in job.__table__.columns.keys()}
+            job_posting_skills = db.session.query(JobPostingSkill, Skill).join(Skill, Skill.id == JobPostingSkill.skill_id).join(JobPosting, JobPostingSkill.job_posting_id == job.id).all()
+            result_posting_skill = []
+            result_job = {i:v for i, v in job.__dict__.items() if i in job.__table__.columns.keys()}
             for category in categories:
                 data_category = {i:v for i, v in category.__dict__.items() if i in category.__table__.columns.keys()}
-                if data_category['id'] == data_job['job_posting_category_id']:
-                    data_job['job_posting_category_name'] = data_category['name']
-                    data.append(data_job)
+                if data_category['id'] == result_job['job_posting_category_id']:
+                    result_job['job_posting_category_name'] = data_category['name']
                 else:
                     continue
+            for key, r in job_posting_skills:
+                # print(dir(r))
+                value = {i:v for i, v in r.__dict__.items() if i in r.__table__.columns.keys()}
+                result_posting_skill.append(value['name'])
+                # print(result_posting_skill)
+            result_job['job_posting_skills'] = result_posting_skill
+            result.append(result_job)
 
-        return json.dumps(data)
+        return json.dumps(result)
     else:
         delta = 7
-        jobs = db.session.query(JobPosting).join(JobPostingCategory).filter(today-JobPosting.posted_date<delta).all()
-        print([{i:v for i, v in r.__dict__.items() if i in r.__table__.columns.keys()} for r in jobs])
+        jobs = db.session.query(JobPosting).filter(today-JobPosting.posted_date<delta).all()
         
-    
-    return render_template('jobads/jobsearch.html', jobs=jobs, categories=categories, titles=titles, csts=csts, jobfunctions=jobfunctions)
+        result = []
+        for job in jobs:
+            job_posting_skills = db.session.query(JobPostingSkill, Skill).join(Skill, Skill.id == JobPostingSkill.skill_id).join(JobPosting, JobPostingSkill.job_posting_id == job.id).all()
+            result_posting_skill = []
+            result_job = {i:v for i, v in job.__dict__.items() if i in job.__table__.columns.keys()}
+            for category in categories:
+                data_category = {i:v for i, v in category.__dict__.items() if i in category.__table__.columns.keys()}
+                if data_category['id'] == result_job['job_posting_category_id']:
+                    result_job['job_posting_category_name'] = data_category['name']
+                else:
+                    continue
+            for key, r in job_posting_skills:
+                # print(dir(r))
+                value = {i:v for i, v in r.__dict__.items() if i in r.__table__.columns.keys()}
+                result_posting_skill.append(value['name'])
+                # print(result_posting_skill)
+            result_job['job_posting_skills'] = result_posting_skill
+            result.append(result_job)
+        
+    return render_template('jobads/jobsearch.html', jobs=result, categories=categories, titles=titles, csts=csts, jobfunctions=jobfunctions)
 
 @app.route('/jobads/searchpeople', methods=['GET', 'POST'])
 @login_required
